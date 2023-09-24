04d3da99977d8f3c4c90325090bb8f25b3036cc6
sasoritheslayer <parkhachev.andrew03@gmail.com>
2023-09-24 18:32:09 +0300

    Edited deploy.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { ACCOUNT_DEPLOYER, API } from "./config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+	solidity: "0.8.19",
+	networks: {
+		goerli: {
+			accounts: [ACCOUNT_DEPLOYER],
+			url: API,
+		},
+	},
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,34 @@
 import { ethers } from "hardhat";
+import { OWNERS, required } from "./owners";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+	const BoxV1 = await ethers.getContractFactory("FirstGeneration");
+	const boxV1 = await BoxV1.deploy();
+	await boxV1.waitForDeployment();
 
-  const lockedAmount = ethers.parseEther("0.001");
+	const BoxV2 = await ethers.getContractFactory("SecondGeneration");
+	const boxV2 = await BoxV2.deploy();
+	await boxV2.waitForDeployment();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+	const Proxy = await ethers.getContractFactory("Proxy");
+	const proxy = await Proxy.deploy();
+	await proxy.waitForDeployment();
 
-  await lock.waitForDeployment();
+	const ProxyAdmin = await ethers.getContractFactory("ProxyAdminMultisig");
+	const proxyAdminMultisig = await ProxyAdmin.deploy(OWNERS, required);
+	await proxyAdminMultisig.waitForDeployment();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+	const addressProxyAdmin = proxyAdminMultisig.target;
+	await proxy.setAdmin(addressProxyAdmin);
+
+	console.log(`Implementation deployed to :${boxV1.target},
+  Proxy deployed to :${proxy.target},
+  ProxyAdmin deployed to :${proxyAdminMultisig.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
